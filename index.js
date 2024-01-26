c23d979efff897fb9a27a8541fad1580f88e42d0
JessCP25 <jpayanoc25@gmail.com>
2024-01-26 06:21:45 -0500

    Routing con Expres.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 })
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola esta es mi nueva ruta');
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+  });
+})
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
