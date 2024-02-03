0ebe08498d7584a826108ac4d01d6b3484ece8df
JessCP25 <jpayanoc25@gmail.com>
2024-02-03 06:25:38 -0500

    Recibir parámetros

--- index.js
+++ index.js
@@ -11,10 +11,32 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 1000
+    },
+    {
+      name: 'Producto 2',
+      price: 2000
+    }
+  ]);
+})
+
+app.get('/products/:id', (req, res) => {
+  const {id} = req.params;
   res.json({
-    name: 'Producto 1',
+    name: id,
     price: 1000
-  });
+  })
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoryId, productId} = req.params;
+  res.json({
+    categoryId,
+    productId
+  })
 })
 
 app.listen(port, () => {
