ad13d92972a6637dc831da67f5c7d228920f8844
JessCP25 <jpayanoc25@gmail.com>
2024-02-08 06:44:30 -0500

    PUT, PATCH, DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -6,20 +6,20 @@ const router = express.Router();
 router.get('/', (req, res) => {
   const products = [];
   const { size } = req.query;
-  const limit = size || 50
+  const limit = size || 50;
   for (let index = 0; index < limit; index++) {
     products.push({
       name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
-      image: faker.image.url()
-    })
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url(),
+    });
   }
   res.json(products);
 });
 
 router.get('/filter', (req, res) => {
   res.send('Yo soy un filtro.');
-})
+});
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
@@ -33,8 +33,26 @@ router.post('/', (req, res) => {
   const body = req.body;
   res.json({
     message: 'created',
-    date: body
-  })
-})
+    data: body,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'Updated',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'Deleted',
+    id,
+  });
+});
 
 module.exports = router;
