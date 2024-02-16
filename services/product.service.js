0b5070bc739c4ed248e9ec78c02e00048aa67a2b
JessCP25 <jpayanoc25@gmail.com>
2024-02-16 17:08:18 -0500

    Middleware para HttpErrors

--- services/product.service.js
+++ services/product.service.js
@@ -21,44 +21,45 @@ class ProductsService {
   async create(data) {
     const newProduct = {
       id: faker.string.uuid(),
-      ...data
-    }
+      ...data,
+    };
     this.products.push(newProduct);
     return newProduct;
   }
 
   find() {
     return new Promise((resolve, reject) => {
-      setTimeout(()=>{
+      setTimeout(() => {
         resolve(this.products);
-      },5000)
-    })
+      }, 5000);
+    });
   }
 
   async findOne(id) {
-    return this.products.find(product => product.id === id)
+    const name = this.getTotal();
+    return this.products.find((product) => product.id === id);
   }
 
   async update(id, changes) {
-    const index = this.products.findIndex(product => product.id === id);
-    if(index === -1) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
       throw new Error('product not found');
     }
     const product = this.products[index];
     this.products[index] = {
       ...product,
-      ...changes
-    }
+      ...changes,
+    };
     return this.products[index];
   }
 
   async delete(id) {
-    const index = this.products.findIndex(product => product.id === id);
-    if(index === -1) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
       throw new Error('product not found');
     }
     this.products = this.products.splice(index, 1);
-    return {id};
+    return { id };
   }
 }
 
